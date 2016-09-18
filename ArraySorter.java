b7995d2c65770e8b354ccd67087fa58e217dd1fa
Sasun Grigoryan <sasungrig@gmail.com>
2016-09-18 13:16:58 +0400

    Selection sort added with tracing functionality

--- ArraySorter.java
+++ ArraySorter.java
@@ -21,14 +21,25 @@ public class ArraySorter {
         System.out.println();
     }
 
+    private void PrintInitialMessage(int[] array, String sortingAlgorithmName, boolean descendingSort)
+    {
+        String sortOrder = descendingSort ? "descending" : "ascending";
+        System.out.print("Is about to sort this array using " + sortingAlgorithmName + " sort by " + sortOrder + " order: ");
+        PrintArray(array);
+    }
+
+    private void PrintTerminalMessage(int[] array)
+    {
+        System.out.print("The final array after sorting is: ");
+        PrintArray(array);
+    }
+
 
     public void SortByInsertionSort(int[] array, boolean descendingSort, boolean traceIntermediateResults)
     {
         if (traceIntermediateResults)
         {
-            String sortOrder = descendingSort ? "descending" : "ascending";
-            System.out.print("Is about to sort this array using insertion sort by " + sortOrder + " order: ");
-            PrintArray(array);
+            PrintInitialMessage(array,"insertion", descendingSort);
         }
         for (int i=1;i<array.length;i++)
         {
@@ -65,13 +76,66 @@ public class ArraySorter {
             {
                 System.out.print("After shifting to the left: ");
                 PrintArray(array, j+1);
-                System.out.println("-----------------------------");
+                System.out.println();
             }
         }
         if (traceIntermediateResults)
         {
-            System.out.print("The final array after sorting is: ");
-            PrintArray(array);
+            PrintTerminalMessage(array);
         }
     }
+
+    public void SortBySelectionSort(int[] array, boolean descendingSort, boolean traceIntermediateResults)
+    {
+        if (traceIntermediateResults)
+        {
+            PrintInitialMessage(array, "selection", descendingSort);
+        }
+        for (int i=0;i<array.length-1;i++)
+        {
+            if (traceIntermediateResults)
+            {
+                System.out.println("Element #" + (i+1));
+                PrintArray(array, i);
+            }
+            int currentNumber = array[i];
+            int swapElement = currentNumber;
+            int swapIndex = i;
+            for (int j = i+1;j<array.length;j++)
+            {
+                boolean shouldSwap;
+                if (descendingSort)
+                {
+                    shouldSwap = array[j] > swapElement;
+                }
+                else
+                {
+                    shouldSwap = array[j] < swapElement;
+                }
+                if (shouldSwap)
+                {
+                    swapElement = array[j];
+                    swapIndex = j;
+                }
+            }
+            if (traceIntermediateResults)
+            {
+                System.out.println("Found swap element: ");
+                PrintArray(array, swapIndex);
+            }
+            array[i] = swapElement;
+            array[swapIndex] = currentNumber;
+            if (traceIntermediateResults)
+            {
+                System.out.println("After swapping: ");
+                PrintArray(array);
+                System.out.println();
+            }
+        }
+        if (traceIntermediateResults)
+        {
+            PrintTerminalMessage(array);
+        }
+    }
+
 }
